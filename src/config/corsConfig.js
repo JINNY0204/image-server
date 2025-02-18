7f4c4fff0d95940559c1e01c637aea4059b0a34c
LEEYOUNGJIN <cuteyj02@gmail.com>
2025-02-19 05:00:26 +0900

    Cors 설정 수정

--- src/config/corsConfig.js
+++ src/config/corsConfig.js
@@ -1,11 +1,20 @@
+// const corsOptions = {
+//     origin: [
+//         'http://localhost:8080'
+//     ],
+//     methods: ['GET', 'POST'],
+//     allowedHeaders: ['Content-Type'],
+//     credentials: true,
+// };
+
 const corsOptions = {
-    origin: [
-        'http://localhost:8080',
-        'http://192.168.0.2:8080'
-    ],
+    origin: function (origin, callback) {
+        callback(null, true);
+    },
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type'],
     credentials: true,
 };
+module.exports = corsOptions;
 
 module.exports = corsOptions;
